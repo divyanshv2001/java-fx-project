daa7a9d341d8bc0dbda91cc0eb9315f3787aab55
Divyansh Vishwakarma <99712584+divyanshv2001@users.noreply.github.com>
2025-04-13 00:07:45 +0530

    update 9

--- start_8/src/application/Main.java
+++ start_8/src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -20,12 +21,20 @@ public class Main extends Application {
 	public void start(Stage stg) throws Exception {
 		Parent root=FXMLLoader.load(getClass().getResource("scene_1.fxml"));
 		Scene scene=new Scene(root);
+		stg.setOnCloseRequest(event -> {
+			event.consume();
+			exit(stg);
+		});
 		stg.setScene(scene);
 		stg.show();
 	}
 	public void exit(Stage stg) {
 		Alert ar=new Alert(AlertType.CONFIRMATION);
 		ar.setTitle("exit confirmation");
+		ar.setContentText("exiting");
+		if(ar.showAndWait().get()== ButtonType.OK) {
+			stg.close();
+		}
 	}
 }
 
